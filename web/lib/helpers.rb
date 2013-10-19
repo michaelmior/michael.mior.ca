efb7d4a15064855e51780d7003c9c9e2c45a5a55
Michael Mior <michael.mior@gmail.com>
2013-10-19 13:35:37 -0400

    Switch to archive pages by year

--- web/lib/helpers.rb
+++ web/lib/helpers.rb
@@ -2,3 +2,9 @@ include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::XMLSitemap
+
+def grouped_articles
+    sorted_articles.group_by do |article|
+        Time.parse(article[:created_at]).year
+    end.sort.reverse
+end
